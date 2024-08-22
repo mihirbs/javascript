fb7b582ee00b22e00a9293edd66bf96307abd44a
mihirbs <mihirsolanki2704@gmail.com>
2024-08-22 10:27:30 +0530

    operators are added

---
+++ operators.js
@@ -0,0 +1,15 @@
+// 77;
+// "Mihir bhai"
+// false
+console.log("operators in js")
+let a = 8;
+let b = 4;
+console.log("a + b = ", a+b)// addition operator is used to add two numbers
+console.log("a - b = ", a-b)// subtraction operator is used to subtract two numbers
+console.log("a / b = ", a/b)// division operator is used to divide two numbers
+console.log("a * b = ", a*b)// multiplication operator is used to multiply two numbers
+console.log("a ** b = ", a**b) // exponential operator is used to find the power of a numbers
+console.log("a % b = ", a%b) // modulus operator is used to find the remainder of
+console.log("a++ = ", a++) //
+console.log("a-- = ", a--) //
+
