41711df285c91c3f5f30a14046c85a9315d986e8
mihirbs <mihirsolanki2704@gmail.com>
2024-08-23 23:53:26 +0530

    new file added conditionalstatements

--- conditionalstatements.js
+++ conditionalstatements.js
@@ -1,1 +1,12 @@
 // to implement some condition in the code
+
+let age = 20;
+const minimumage = 18;
+
+if (age >= minimumage){
+    console.log("you are eligible to vote fot the election");
+}
+
+if (age < minimumage) {
+    console.log("you are not eligible to vote in the election");
+}
