29e4f80510da9e046b31c51515a39d663566bff6
mihirbs <mihirsolanki2704@gmail.com>
2024-08-23 16:20:25 +0530

    product added it is an example of object

---
+++ productobject.js
@@ -0,0 +1,10 @@
+const product = {
+    title: "Graphic card",
+    rating: 4.5,
+    price: 33000,
+    description: "This is a graphic card",
+    image: "https://example.com/image.jpg"
+    
+};
+
+console.log(product);
