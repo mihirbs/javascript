cf75f477c99b35d993dcbaf39774d2b53f114f34
mihirbs <mihirsolanki2704@gmail.com>
2024-08-23 16:15:49 +0530

    added objects file

---
+++ object.js
@@ -0,0 +1,12 @@
+// object means collection of different variables
+
+// here is an example
+//in object there are key value pairs like this (name: mihir)
+const intern = {
+fullName: "Mihir Brijesh Solanki",
+age: 20,
+cgpa: 8.28,
+position: "Backend developer",
+learning: "MERN stack",
+
+}
