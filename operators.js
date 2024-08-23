9b21244f42d2e2b3a5f796b13b332145144ed690
mihirbs <mihirsolanki2704@gmail.com>
2024-08-23 16:52:33 +0530

    new files were added like oprators and script

---
+++ operators.js
@@ -0,0 +1,10 @@
+// Arithmeatic operators
+let a = 58
+let b = 25
+console.log("a + b = ",a + b); // Output: 83
+console.log("a - b = ",a - b);// Output: 33
+console.log("a * b = ",a * b); // Output: 1450
+console.log("a / b = ",a / b); // Output: 2.32
+console.log("a % b = ",a % b); // Output: 3 its called modulus and it gives reminder of the division of the given values
+console.log("a =", a , "and b =", b); // output: a b
+console.log("a ** b = ", a ** b);  // this called exponentiation like in maths we use a
---
+++ script.js
@@ -0,0 +1,5 @@
+// this is a single line comment
+
+console.log("this file is about comments ");
+console.log("for single line comment use this //");
+console.log("for multi line comment use this /* */");
