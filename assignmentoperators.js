2867164272ee9fdf9ceb95a29cc6fa8f286a5acb
mihirbs <mihirsolanki2704@gmail.com>
2024-08-23 17:32:40 +0530

    added the file of the assignment operatror

---
+++ assignmentoperators.js
@@ -0,0 +1,23 @@
+// all about the assignment operators
+// =, +=, -=, *=, /=, %=, **=, <<=,
+
+let a = 3; // this = operator is used to assign value 
+let b = 5; // same applies here
+a += 4 // it means that a + 4 which is 7
+console.log("a = ",a);
+console.log("b = ",b);
+// -=, *=, /=, %=, **=, <<=, >>=, >>>=, &=
+// -=, *=, /=, %=, **=, <<=, >>=, >>>=, &=
+a -= 4 // it means that a - 4 which is 3
+console.log("a = ",a);
+// **= is used to calculate the power of a number
+a **= 2 // it means that a * a which is 9
+console.log("a = ",a);
+// <<=, >>=, >>>=, &=, ^=, |=
+// <<=, >>=, >>>=, &=, ^=, |=
+a <<= 2 // it means that a << 2 which is 36
+console.log("a = ",a);
+// ^=, |=
+a ^= 2 // it means that a ^ 2 which is 34
+console.log("a = ",a);
+// |=
