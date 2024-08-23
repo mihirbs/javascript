a2d8dabb500c9317da8990b9bdc15fa1e3beb42f
mihirbs <mihirsolanki2704@gmail.com>
2024-08-23 17:23:59 +0530

    uninanry operator is added in the operators file

--- operators.js
+++ operators.js
@@ -1,10 +1,42 @@
 // Arithmeatic operators
-let a = 58
-let b = 25
-console.log("a + b = ",a + b); // Output: 83
-console.log("a - b = ",a - b);// Output: 33
-console.log("a * b = ",a * b); // Output: 1450
-console.log("a / b = ",a / b); // Output: 2.32
-console.log("a % b = ",a % b); // Output: 3 its called modulus and it gives reminder of the division of the given values
-console.log("a =", a , "and b =", b); // output: a b
-console.log("a ** b = ", a ** b);  // this called exponentiation like in maths we use a
+// let a = 58
+// let b = 25
+// console.log("a + b = ",a + b); // Output: 83
+// console.log("a - b = ",a - b);// Output: 33
+// console.log("a * b = ",a * b); // Output: 1450
+// console.log("a / b = ",a / b); // Output: 2.32
+// console.log("a % b = ",a % b); // Output: 3 its called modulus and it gives reminder of the division of the given values
+// console.log("a =", a , "and b =", b); // output: a b
+// console.log("a ** b = ", a ** b);  // this called exponentiation like in maths we use 58^25 and the output will be a ** b =  1.2181473901262644e+44
+
+// to commet out the existing code use ctrl + / 
+
+// uninary operator
+
+ let a = 10
+ let b = 5
+
+console.log("a = ",a,"& b = ",b);
+a = a+1 ; // 11
+console.log("a = ",a);
+
+a = a++ ; // both are same it will increment a with 1 and the upper one will also do the same   
+// its also called incremet which will be further used in the loops and in the conditional statmets
+
+b = b-1 ;
+console.log("b = ",b);
+
+b = b-- ; // this called decrement similar to the above code only diffrence in this that it will - 1 from the value of bthat variable like b--
+console.log("b = ",b); // 4
+
+// a++ (post increment) a-- (post decrement)
+// ++a (pre increment) b-- (pre decrement)
+// for example
+
+ console.log("++a = ", ++a); // it will icrement the value and then print
+ console.log("a = ", a); // 12
+ console.log("a++ = ", a++);// it will print the value first and then it will in crement it from the next line
+ console.log("a = ", a); // 12
+
+ // as well as decrement is also work like that but it uses a-- or b-- for example
+ 
