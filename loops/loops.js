1f1d42b6920db9e2dfe8f490f4f5270b283df0f0
mihirbs <mihirsolanki2704@gmail.com>
2024-08-29 19:35:09 +0530

    new programs added of the other types of loops

--- loops/loops.js
+++ loops/loops.js
@@ -24,3 +24,46 @@ function generateNumbers() {
     // it will continue to increment or decrement until the condition is not true
     
 }
+
+// example of the while loop
+{
+    // Program to find the sum of numbers from 1 to 100 using a while loop
+let sum = 0;
+let i = 1;
+while (i <= 100) {
+    sum += i;
+    i++;
+}
+console.log(`Sum: ${sum}`);
+
+}
+// example of the do while loop 
+
+{
+    // Program to print the first 5 even numbers using a do-while loop
+let i = 1;
+let count = 0;
+do {
+    if (i % 2 === 0) {
+        console.log(i);
+        count++;
+    }
+    i++;
+} while (count < 5);
+
+}
+// example of the for in loop (it is not used that much)
+
+{
+    // Program to print the properties and values of an object using a for...in loop
+const person = {
+    name: "Leo",
+    age: 21,
+    city: "Mumbai"
+};
+for (let key in person) {
+    console.log(`${key}: ${person[key]}`);
+}
+
+}
+
