785a1a6ddcf2a9dbe6113bbf9c8d7d96598e328f
mihirbs <mihirsolanki2704@gmail.com>
2024-08-29 17:27:34 +0530

    changes in the loops.js file

--- loops/loops.js
+++ loops/loops.js
@@ -11,6 +11,7 @@ function generateNumbers() {
     numberList.innerHTML = '';
 
     // Generate numbers from 1 to the entered number using a loop
+    // using for loop
     for (let i = 1; i <= number; i++) {
         // Create a new list item (li) for each number
         const listItem = document.createElement('li');
@@ -19,4 +20,7 @@ function generateNumbers() {
         // Add the list item to the number list
         numberList.appendChild(listItem);
     }
+    // at first we do the initialization i = 1 , then we will delare the condition , it will increment or decrement as per the user declaration in the loop  by i++ and i--
+    // it will continue to increment or decrement until the condition is not true
+    
 }
