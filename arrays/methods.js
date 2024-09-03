170f56aa28ba717d50a96e7c1fd636c74462120c
Shivanggithub <shivangpatel0118@gmail.com>
2024-09-03 17:00:30 +0530

    methods of array

---
+++ arrays/methods.js
@@ -0,0 +1,47 @@
+// slice =  Returns a portion of an array without modifying the original array.
+{
+let slicedArray = fruits.slice(0, 2);
+console.log(slicedArray); // Outputs: ["Mango", "Orange"]
+}
+//concat = it is used to combine two or more arrays into one
+
+
+{
+    let moreFruits = ["Pineapple", "Strawberry"];
+let combinedFruits = fruits.concat(moreFruits);
+console.log(combinedFruits); // Outputs: ["Mango", "Orange", "Pineapple", "Strawberry"]
+
+}
+
+//creating an array
+{
+
+let fruits = ["Apple", "Banana", "Orange"];
+console.log(fruits);
+}
+
+// accesing array elements 
+
+{
+    console.log(fruits[0]); // Outputs: Apple
+console.log(fruits[1]); // Outputs: Banana
+console.log(fruits[2]); // Outputs: Orange
+
+}
+
+// changinf elements in array
+
+{
+    fruits[1] = "Mango";
+console.log(fruits); // Outputs: ["Apple", "Mango", "Orange"]
+
+}
+
+// array length
+
+{
+    fruits[1] = "Mango";
+console.log(fruits); // Outputs: ["Apple", "Mango", "Orange"]
+
+}
+
