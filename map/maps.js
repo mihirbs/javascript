402ef9d17116a3726ef71e73fd2c416510c13e9b
Shivanggithub <shivangpatel0118@gmail.com>
2024-09-04 17:14:12 +0530

    map is added

--- map/maps.js
+++ map/maps.js
@@ -5,3 +5,45 @@
 let map = new Map();
 
 // adding key value pairs
+
+map.set('name','Leo');
+map.set('age',21);
+map.set(true,'Boolean key');
+map.set({x: 10},'object key');
+
+// Acessing values:
+
+console.log(map.get('name'));
+console.log(map.get(true));
+
+//checking for the keys
+
+console.log(map.has('age'));
+
+//Removing a key value pair
+
+map.delete('age');
+console.log(map.has('age'));
+
+// iterarting over a map 
+
+for (let[key, value]of map) {
+    console.log(key,value);
+}
+
+// getting the size of map
+
+console.log(map.size);
+
+// clearing the map
+
+map.clear();
+console.log(map.size);
+
+const fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+  ]);
+
+  
